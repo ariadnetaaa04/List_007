bc7dce9eab20f7b438355b54cbdfbf5edc86d7f9
ariadnetaaa04 <ariadna.perez.0401@gmail.com>
2023-02-13 10:13:57 +0100

    13-02

--- Assets/Scripts/Song.cs
+++ Assets/Scripts/Song.cs
@@ -8,10 +8,24 @@ public class Song : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        while (bottles > 0)
+        while (bottles >= 2)
         {
-            Debug.Log($"{bottles} bottles of beer on the wall, {bottles} bottles of beer. Take one down and pass it around, {bottles - 1} bottles of beer on the wall.");
-         }
+            Debug.Log($"{bottles} bottles of beer on the wall, {bottles} bottles of beer. ");
+            bottles=bottles - 1;
+            Debug.Log($"Take one down and pass it around, {bottles} bottles of beer on the wall.");
+
+            if (bottles == 1)
+            {
+                Debug.Log("1 bottle of beer on the wall, one bottle of beer. Take one down, pass it around, 1 bottle of beer on the wall.");
+                bottles = bottles - 1;
+            }
+            if(bottles == 0)
+            {
+                Debug.Log("No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.");
+            }
+        }
+        
+        
     }
 
     // Update is called once per frame
